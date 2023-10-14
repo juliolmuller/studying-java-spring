305f430fbc0a331ea48d8c4674582a2e056c7d36
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2023-10-14 14:41:30 -0300

    chore: initialize Spring project

---
+++ src/test/java/com/juliolmuller/todo/ToDoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.juliolmuller.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
