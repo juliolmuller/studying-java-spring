5361942c51a01d270b66b10e8fe3fdc711af1019
Julio L. Muller <julio_muller@crgl-thirdparty.com>
2023-10-16 00:05:25 -0300

    feat: create endpoint to update tasks

--- src/main/java/com/juliolmuller/todo/controller/TaskController.java
+++ src/main/java/com/juliolmuller/todo/controller/TaskController.java
@@ -1,13 +1,14 @@
 package com.juliolmuller.todo.controller;
 
-import java.util.UUID;
-
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,25 @@ public class TaskController {
         var taskCreated = this.taskRepository.save(task);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity update(HttpServletRequest request, @PathVariable UUID taskId, @RequestBody Task task) {
+        var userId = (UUID) request.getAttribute("userId");
+        var existingTask = this.taskRepository.findById(taskId).get();
+
+        if (existingTask == null || !existingTask.getUserId().equals(userId)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        existingTask.setTitle(task.getTitle());
+        existingTask.setDescription(task.getDescription());
+        existingTask.setPriority(task.getPriority());
+        existingTask.setDueDate(task.getDueDate());
+        existingTask.setDone(task.isDone());
+
+        var updatedTask = this.taskRepository.save(existingTask);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
     }
 }
